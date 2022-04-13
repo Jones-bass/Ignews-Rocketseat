ec78a781034576b9d9f42055a8d1f6ac9ca29cc2
Jones-bass <jonesbass.tb@gmail.com>
2022-04-13 13:39:08 -0300

    Title dinamico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <div>Ola</div>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <div>Ola BB</div>
+    </>
+  );
 }

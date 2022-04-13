91529e50c14bc6188c9b9b94239ef1d3128e76dc
Jones-bass <jonesbass.tb@gmail.com>
2022-04-13 15:08:00 -0300

    Estilizando Home e Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton_";
+import styles from "../pages/home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +8,22 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <div>Ola BB</div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            {" "}
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding." />
+      </main>
     </>
   );
 }

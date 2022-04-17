d4dfea9b7c6358efed49e855fe5feb008955396a
Jones-bass <jonesbass.tb@gmail.com>
2022-04-16 22:32:06 -0300

    listando prismic

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Engineer innovative e-services","spans":[]}],"description":[{"type":"paragraph","text":"Fugiat proident quis enim velit aute excepteur dolore sint sint Lorem aliqua elit consectetur culpa.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,43 +1,69 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Prismic from '@prismicio/client';
-import styles from './styles.module.scss';
 import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-reactjs';
 
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
 
-export default function Posts() {
-  return (
-    <>
-      <Head>
-        <title>Posts | Ignews</title>
-      </Head>
+interface PostsProps {
+    posts: Post[];
+}
 
-      <main className={styles.container}>
-        <div className={styles.posts}>
-          <a href="#"></a>
-          <time>15 de abril de 2022</time>
-          <strong>Jones Bass Testando</strong>
-          <p>Hum vamos ao conteudo</p>
-        </div>
-      </main>
-    </>
-  );
+export default function Posts({ posts }: PostsProps) {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map((post) => (
+                        <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
+                </div>
+            </main>
+        </>
+    );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient();
+    const prismic = getPrismicClient()
 
-  const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'publication')
-  ], {
-    fetch: ['publication.title', 'publication.content'],
-    pageSize: 100,
-  });
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'ignews')
+    ], {
+        fetch: ['ignews.title', 'ignews.content'],
+        pageSize: 100,
+    })
 
-  console.log(response)
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+            })
+        }
+    })
 
-  return {
-    props: {
+    return {
+        props: { posts },
     }
-  }
 }
+
+//yarn add @prismicio/client@5.1.1

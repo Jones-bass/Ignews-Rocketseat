1df79b13913c90579efdae794b465604b3177353
Jones-bass <jonesbass.tb@gmail.com>
2022-04-17 12:58:04 -0300

    Pagina Preview

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,7 +40,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  
+   console.log(session)
+
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID('ignews', String(slug), {});
